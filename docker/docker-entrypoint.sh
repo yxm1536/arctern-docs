54b3a899567e243ed45ac4f7e9dcdf8da3d12652
beautifulpython <2426009680@qq.com>
2020-06-23 15:54:20 +0800

    add &

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -16,11 +16,11 @@ pip install pyspark
 cd / && echo "--------" && tree /arctern-docs && echo "--------" && \
 cd /arctern-docs/doc-cn && \
 mkdir build && python create_html.py && mv build build-cn &&\
-git config --global user.email "Arctern-doc-bot@zilliz.com"
-git config --global user.name "Arctern-doc-bot"
-git status
-git remote -v
-git branch
+git config --global user.email "Arctern-doc-bot@zilliz.com" && \
+git config --global user.name "Arctern-doc-bot"&& \
+git status && \
+git remote -v && \
+git branch && \
 cd /arctern-docs && \
 git checkout -b `cat version.json | jq -r .version` && git add . && \
 git commit -m "Arctern-bot release doc" && \
